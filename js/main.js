b32de5cab0dc1c5708e38e415e660b64c48e90e0
Hupewolf <nhannguyen.ix5@gmail.com>
2025-11-30 18:58:37 +0700

    testing form: hoàn thành

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
 const form = document.getElementById("formDK");
-const resultList = document.getElementById("resultList");
+const resultList = document.getElementById("result");
 
 form.addEventListener("submit", function(e) {
     e.preventDefault();
 
     // Lấy dữ liệu từ input
-    const name = document.getElementById("fullname").value;
+    const first = document.getElementById("first").value;
+    const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const phone = document.getElementById("phone").value;
     const note = document.getElementById("note").value;
 
     // Tạo một khối mới
     const box = document.createElement("div");
-    box.className = "item-box";
+    box.className = "list-container-table-row";
 
     box.innerHTML = `
-        <p><strong>Họ tên:</strong> ${name}</p>
-        <p><strong>Email:</strong> ${email}</p>
-        <p><strong>Số điện thoại:</strong> ${phone}</p>
-        <p><strong>Ghi chú:</strong> ${note}</p>
+        <p class="list-container-table-row-text name">${first} ${name}</p>
+        <p class="list-container-table-row-text email">${email}</p>
+        <p class="list-container-table-row-text phone">${phone}</p>
+        <p class="list-container-table-row-text desc">${note}</p>
     `;
 
     // Thêm vào danh sách
